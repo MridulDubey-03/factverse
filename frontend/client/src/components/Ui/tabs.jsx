b7831195be3bfac9d7800d7d054a44370c212df7
MridulDubey-03 <mridulddubey@gmail.com>
2025-09-12 01:39:39 +0530

    resolving errors

--- frontend/client/src/components/Ui/tabs.jsx
+++ frontend/client/src/components/Ui/tabs.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 
-import { cn } from "./utils.js";
+import { cn } from ".ui//utils.js";
 
 const Tabs = TabsPrimitive.Root;
 
